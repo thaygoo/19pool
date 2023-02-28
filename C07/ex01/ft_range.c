c96813bfb32d2ba759ea257c9d0a7ce374e5334c
hburton <hburton@student.s19.be>
2023-02-28 13:55:57 +0100

    ft_range check

--- C07/ex01/ft_range.c
+++ C07/ex01/ft_range.c
@@ -6,7 +6,7 @@
 /*   By: hburton <hburton@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/28 12:33:39 by hburton           #+#    #+#             */
-/*   Updated: 2023/02/28 12:37:09 by hburton          ###   ########.fr       */
+/*   Updated: 2023/02/28 13:54:18 by hburton          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,34 @@
 int	*ft_range(int min, int max)
 {
 	int	*table;
+	int	i;
 
-	table = malloc()
+	i = 0;
+	if (min >= max)
+		return (NULL);
+	table = malloc((max - min) * sizeof(int));
+	if (!table)
+		return (NULL);
+	while ((max - min) > i)
+	{
+		table[i] = min + i;
+		i++;
+	}
+	return (table);
 }
 
+/**
 #include <stdio.h>
 
 int	main(void)
 {
-	
+	int	i;
+
+	i = 0;
+	while (i < 9)
+	{
+		printf("%d \n", ft_range(10, 20)[++i]);
+	}
 	return (0);
 }
+**/
